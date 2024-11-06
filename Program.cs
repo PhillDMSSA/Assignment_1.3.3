1ab7f90b5901eb164a10f3d99c4c9fe81051d149
phillip deleon <phillipdeleon68@gmail.com>
2024-11-06 10:06:56 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Assignment_1._3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[3];
+            nums[0] = 2;
+            nums[1] = 5;
+            nums[2] = 7;
+
+            Console.WriteLine("The values store into the array are:");
+            foreach ( int num in nums)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine();
+
+            Array.Reverse(nums); //reserves Array
+
+            Console.WriteLine("The values store into the array in reverse are:");
+            foreach (int num in nums)
+            {
+                Console.Write(num + " ");
+            }
+
+            
+
+
+
+
+        }
+    }                                                                           
+}
+                                                     
